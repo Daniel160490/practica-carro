e780dd257efc04c825bcd469dd552c00b596f965
Daniel Garcia Diaz <daniel@Air-de-Daniel.home>
2018-11-08 22:44:14 +0100

    Practica Carro de compra

---
+++ CarroCompra/src/es/indra/carrocompra/main/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.indra.carrocompra.main;
+
+import es.indra.carrocompra.sistema.Sistema;
+
+/**
+ * @author daniel
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Sistema sistema = new Sistema();
+		sistema.menuPrincipal();
+		
+
+	}
+
+}
